62b92a52cbf7c519d584604a02f2a4503ad00f84
Alex Breathnach <k00297199@student.tus.ie>
2024-10-25 13:58:40 +0100

    Add files via upload

---
+++ CA Files-20241025/CA Files/script.js
@@ -0,0 +1,40 @@
+//1. fetch data from a public api
+//2. use lorem ipsum api to generate images
+document.getElementById('fetchDataButton'); addEventListener(onclick('click'));
+{
+    // Function
+    // myFunction()
+    // {
+    //     let fetch = (picsum.photos / 400 / 300 ? random = $Math.random();
+    // )
+    //     then(Response => response.json)
+    //     return DataButton;
+    // }
+    fetch(https:picsum.photos/400/300?random=${Math.random()})
+
+
+
+
+}
+//3. random image url = (https://picsum.photos/400/300?random=${Math.random()})
+
+//4. display the data inside a dataContainer div{
+//do this in the index file}
+.
+    container
+    {
+
+        onload('slide', screenLeft);
+        addEventListener()
+
+
+        addEventListener('click', onclick)
+    }
+}
+//5. animate the data to slide in from the left when loaded
+
+//6. add a button click event trigger
+
+
+
+//7. implement additional interaction
